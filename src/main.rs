1753e8bf6c4c0521c818c6546eeb59cb19b27c7b
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-09-12 03:53:00 +0500

    L1-1 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+///
+/// Public trait action wit method say
+/// 
+pub trait Action {
+    fn say(&self);
+}
+
+///
+/// Structure Person with public field name as String
+/// 
+pub struct Person {
+    pub name: String
+}
+
+///
+/// Implementation of trait Action for Person
+/// 
+impl Action for Person {
+    fn say(&self) {
+        println!("Hello, {}", self.name)
+    }
+}
+
+///
+/// Implementation of struct Person with method new
+/// 
+impl Person {
+    fn new(name: String) -> Self {
+        Person { name: name }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let name = "PawKorchagin";
+    let person = Person::new(name.to_string());
+
+    person.say();
 }
