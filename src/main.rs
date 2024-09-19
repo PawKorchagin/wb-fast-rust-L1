1e9cb9988d85cf108fc28b2d9e3d23d6ecbf35a7
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-09-19 23:37:45 +0300

    feat: hashset

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::HashSet;
+
+fn has_unique_characters(s: &str) -> bool {
+    let mut seen = HashSet::new();
+
+    // Iterates over the characters, converting them to lowercase
+    for c in s.chars()
+        .map(|c| 
+            c.to_lowercase()
+                .next()
+                .unwrap()) {
+        if !seen.insert(c) {
+            return false;
+        }
+    }
+
+    true
+}
+
 fn main() {
-    println!("Hello, world!");
+    let test_cases = vec![
+        "abcd",       // 1
+        "abCdefAaf",  // 0
+        "aabcd",      // 0
+    ];
+
+    for test in test_cases {
+        let result = has_unique_characters(test);
+        println!("{} — {}", test, result);
+    }
 }
