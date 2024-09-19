a5ff72a4dbfc47510b86b6eff9744306d87edd51
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-09-19 23:54:10 +0300

    feat: sequence channels

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx1, rx2) = mpsc::channel();
+    let (tx2, rx3) = mpsc::channel();
+
+    let mut handles = Vec::new();
+
+    let handle_square = thread::spawn(move || {
+        for num in rx2 {
+            tx2.send(num * num).unwrap();
+        }
+    });
+
+    handles.push(handle_square);
+
+    let handle_print = thread::spawn(move || {
+        for square in rx3 {
+            println!("{}", square);
+        }
+    });
+
+    handles.push(handle_print);
+
+    const N: usize = 10;
+    let mut numbers: [u32; N] = [0; N];
+
+    for i in 0..N {
+        numbers[i] = (i + 1) as u32;
+    }
+
+    for &number in &numbers {
+        tx1.send(number).unwrap();
+    }
+    drop(tx1); 
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
