e1d1fbe38ab78ac9afaa0d4f0205a68cb9212f91
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-09-19 23:28:30 +0300

    feat: reversing sequence of words

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let s = "snow dog sun";
+
+    // Fold is a consuming iterator adaptor which applies a function to each element of the iteration, accumulating the result into a new value.
+
+    println!("{}", s.split_whitespace().fold(String::new(), |acc, word| {
+        if acc.is_empty() {
+            word.to_string()
+        } else {
+            format!("{} {}", word, acc)
+        }
+    }));
 }
