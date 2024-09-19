362242095855f4ae176f7eb76cbb356dbc531dc9
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-09-19 23:39:29 +0300

    feat: removing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let mut v = vec![10, 20, 30, 40, 50];
+    
+    let removed_element = v.remove(2);
+
+    println!("Removed element: {}", removed_element);
+    println!("Updated vector: {:?}", v);
 }
