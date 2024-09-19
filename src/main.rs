353882c7452e2fe6da63d53c7bac6e390d9bde77
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-09-19 23:46:34 +0300

    feat: binary search

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let sorted_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let target = 5;
+    match sorted_array.binary_search(&target) {
+        Ok(index) => println!("Element {} found at index {}", target, index),
+        Err(_) => println!("Element {} not found", target),
+    }
+
+    let target_not_found = 11;
+    match sorted_array.binary_search(&target_not_found) {
+        Ok(index) => println!("Element {} found at index {}", target_not_found, index),
+        Err(_) => println!("Element {} not found", target_not_found),
+    }
 }
