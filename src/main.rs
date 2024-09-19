2b30845634a6cf3a1dbe8a8afbfccfab60eeefc5
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-09-19 23:03:12 +0300

    feat: unique strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let mut unique_strings = Vec::new();
+
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+
+        if !unique_strings.contains(&line) {
+            unique_strings.push(line);
+        }
+    }
+
+    // Note: use ctrl + D to end input
+    
+    for line in unique_strings {
+        println!("{}", line);
+    }
 }
