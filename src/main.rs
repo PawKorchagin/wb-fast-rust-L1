6ae6ea9390bf383079891825b3382827c45bdc56
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-09-19 23:50:22 +0300

    feat: sets intersection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let set1: HashSet<i32> = [1, 2, 3, 4, 5].iter().cloned().collect();
+    let set2: HashSet<i32> = [4, 5, 6, 7, 8].iter().cloned().collect();
+
+    let intersection: HashSet<i32> = set1.intersection(&set2).cloned().collect();
+
+    println!("Intersection of sets set1 and set2 is: {:?}", intersection);
 }
