e06901e2cc640e3be9897bdebb257867f8d4e37f
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-09-19 23:09:21 +0300

    feat: sorting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn quicksort_std(arr: &mut [i32]) {
+    if arr.len() <= 1 {
+        return; // Base case: arrays of size 0 or 1 are already sorted
+    }
+
+    // Use the built-in sort method on the slice, which is highly optimized
+    arr.sort_unstable(); // Order stability isn't needed
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut array = [3, 6, 8, 10, 1, 2, 1, 5, 7, 9];
+    println!("Original array: {:?}", array);
+
+    array.sort_unstable();
+
+    println!("Sorted array: {array:?}");
 }
