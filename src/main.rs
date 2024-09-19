e58e94697a4680909649c7052f5fa4c128ce2f36
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-09-19 23:45:16 +0300

    feat: reverse string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn reverse_string(s: &str) -> String {
+    s.chars().rev().collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = "главрыба";
+    println!("Original: {}", input);
+    println!("Reversed: {}", reverse_string(input));
 }
